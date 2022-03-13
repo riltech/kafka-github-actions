042b4926ea994b7af708836fba38f9c166974188
Imre 'Rover' Racz <racz.imre.levente@gmail.com>
2022-03-13 17:59:35 +0100

    fix(test): admin now should create the topic before testing

--- test.js
+++ test.js
@@ -8,28 +8,47 @@ const kafka = new Kafka({
 });
 
 test("When the service is initialised it can connect to Kafka successfully", async (t) => {
-  t.plan(1);
+  t.plan(2);
   const service = new Service();
   service.producer.on("producer.connect", (e) => {
-    t.ok(e.id);
+    t.equal(e.id, 0);
+  });
+  service.producer.on("producer.disconnect", (e) => {
+    t.equal(e.id, 1);
   });
   await service.connect();
+  await service.dispose();
 });
 
 test("Service can publish messages to Kafka successfully ", async (t) => {
   t.plan(2);
+  const admin = kafka.admin();
+  await admin.connect().then(() =>
+    admin.createTopics({
+      topics: [{ topic: "test-topic" }],
+    })
+  );
+
   const service = new Service();
   await service.connect();
   await service.sendMessage("test-topic", [{ value: "foo_bar" }]);
 
-  const consumer = kafka.consumer();
+  const consumer = kafka.consumer({ groupId: "consumer-1" });
   await consumer.connect();
   await consumer.subscribe({ topic: "test-topic", fromBeginning: true });
 
-  await consumer.run({
+  let messageRead;
+  let read = new Promise(res => messageRead = res);
+  consumer.run({
     eachMessage: async ({ topic, _, message }) => {
       t.equal(topic, "test-topic");
-      t.equal(message, "foo_bar");
+      t.equal(message.value.toString(), "foo_bar");
+      messageRead();
     },
   });
+  await read;
+
+  await service.dispose();
+  await admin.disconnect();
+  await consumer.disconnect();
 });
